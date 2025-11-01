39dc9a3dbf844699e7dc417b01deff3c521da0fb
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-11-01 17:36:08 -0500

    add biomes to overworld tag for compat

---
+++ kubejs/server_scripts/worldgen/tags.js
@@ -0,0 +1,12 @@
+ServerEvents.tags('worldgen/biome', e=> {
+    e.add('minecraft:is_overworld', [
+        'splendid_ranching:ancient_ruins',
+        'splendid_ranching:dry_reef',
+        'splendid_ranching:indigo_quarry',
+        'splendid_ranching:magma_mounts',
+        'splendid_ranching:moss_blanket',
+        'splendid_ranching:opal_desert',
+        'splendid_ranching:slime_sea',
+        'splendid_ranching:the_wilds'
+    ])
+})
