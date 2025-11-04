535372ec595f4dbe169e6cad94a55c3d85b1007b
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-11-03 20:48:49 -0600

    wip slime breeder

--- kubejs/server_scripts/slime_scripts/slimeData.js
+++ kubejs/server_scripts/slime_scripts/slimeData.js
@@ -6,109 +6,127 @@ global.baseSlimeData = {
         baseValue: 50,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     bitwise: {
         baseValue: 51,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     blazing: {
         baseValue: 52,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     bony: {
         baseValue: 53,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     boomcat: {
         baseValue: 54,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     ender: {
         baseValue: 55,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     gold: {
         baseValue: 56,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     luminous: {
         baseValue: 57,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     minty: {
         baseValue: 58,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     orby: {
         baseValue: 59,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     phantom: {
         baseValue: 60,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     prisma: {
         baseValue: 61,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     puddle: {
         baseValue: 62,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     rotting: {
         baseValue: 63,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     shulking: {
         baseValue: 64,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     slimy: {
         baseValue: 65,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     sweet: {
         baseValue: 66,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     webby: {
         baseValue: 67,
         currentMultiplier: 1,
         minMultiplier: 0.5,
-        maxMultiplier: 2
+        maxMultiplier: 2,
+        slimeDupeCost: 32
     },
     weeping: {
         baseValue: 68,
