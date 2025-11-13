ebe14ff16645ec61e229c92c86fdc330362dda89
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-11-13 01:27:50 -0600

    improve plort tooltips, add debug info with ctrl

--- kubejs/client_scripts/valueTooltips.js
+++ kubejs/client_scripts/valueTooltips.js
@@ -7,38 +7,39 @@ NetworkEvents.dataReceived('kubejs:slime_value_data', e => {
 // Plort dynamic valuing tooltips
 ItemEvents.tooltip(e => {
     e.addAdvanced(`splendid_slimes:plort`, (item, advanced, text) => {
-        text.remove(1)
-        function addPercent(text, mult) {
-            if (mult != 0 && mult != undefined) {
-                text.add(2, [
-                    `${mult < 0 ? '§c' : '§a+'}${mult}%`
-                ])
-            }
-        }
-
         try {
-            if (!item.nbt || !item.nbt['plort']) return
+            if (!item.nbt || !item.nbt['plort']) return // return if plort item has no nbt (base item)
+            text.remove(1) // remove the default "from fed slimes" tool tip
 
-            let plort = item.nbt['plort'].id.split(':')[1]
+            let plort = item.nbt['plort'].id.split(':')[1] // get plort breed
 
-            if (slimeData && slimeData[plort] === undefined) return
+            if (slimeData && slimeData[plort] === undefined) return // return if plort breed has no data entry
 
             let plortData = slimeData[plort]
             let cost = plortData.currentValue
             let mult = plortData.multPercent
 
-            if (e.shift) {
-                text.add(1, [
-                    `§6${global.calculateCost(cost, 1, item.count)}§a☻`,
-                    item.count > 1 ? '§7 Stack Value' : ''
+            text.add(text.length, [
+                `§6${global.calculateCost(cost, 1, 1)}§a☻`,
+            ])
+            if (item.count > 1) {
+                text.add(text.length, [
+                    `§7Stack Value: §6${global.calculateCost(cost, 1, item.count)}§a☻`
                 ])
-                addPercent(text, mult)
-            } else {
-                text.add(1, [
-                    `§6${global.calculateCost(cost, 1, 1)}§a☻`,
-                    item.count > 1 ? '§8 [§7Shift§8]' : ''
+            }
+            if (mult != 0 && mult != undefined) {
+                text.add(text.length, [
+                    `${mult < 0 ? '§c' : '§a+'}${mult}%`
                 ])
-                addPercent(text, mult)
+            }
+
+            if (e.ctrl) { // show all plort data on ctrl (For debug mostly)
+                let plortDataArr = Object.entries(plortData)
+                for (let [key, val] of plortDataArr) {
+                    text.add(3 + plortDataArr.indexOf(key), [
+                        `${key}: §6${val}`
+                    ])
+                }
             }
         } catch (err) {
             console.log(err)
