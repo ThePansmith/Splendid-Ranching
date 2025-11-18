d9a05b54c389f39cf4d1de157cafa2ce049f26cb
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-11-18 03:46:29 -0600

    update initial gamerules script

--- kubejs/server_scripts/gamerules.js
+++ kubejs/server_scripts/gamerules.js
@@ -1,9 +1,14 @@
-ServerEvents.loaded(event => {
-    event.server.gameRules.set("doTraderSpawning", false)
-    event.server.gameRules.set("doInsomnia", false)
-    event.server.gameRules.set("doPatrolSpawning", false)
-    event.server.gameRules.set("doDaylightCycle", false)
-    event.server.gameRules.set("keepInventory", true)
-    event.server.gameRules.set("disableRaids", true)
-    event.server.gameRules.set("playersSleepingPercentage", 50)
+// first server load 
+ServerEvents.loaded(e => {
+    if (e.server.persistentData['started'] == false) {
+        //  setting game rules
+        e.server.gameRules.set("doTraderSpawning", false)
+        e.server.gameRules.set("doInsomnia", false)
+        e.server.gameRules.set("doPatrolSpawning", false)
+        e.server.gameRules.set("doDaylightCycle", false)
+        e.server.gameRules.set("keepInventory", true)
+        e.server.gameRules.set("disableRaids", true)
+        e.server.gameRules.set("playersSleepingPercentage", 50)
+    }
+    e.server.persistentData['started'] = true
 })
