3b9968660210c8904121b2cc287107c18b50d3cb
Davey <meeprdib@outlook.com>
2025-11-18 11:53:23 -0500

    wrong gamerule :pleading_face:

--- kubejs/server_scripts/firstServerLoad.js
+++ kubejs/server_scripts/firstServerLoad.js
@@ -6,7 +6,7 @@ ServerEvents.loaded(e => {
         e.server.gameRules.set("doTraderSpawning", false)
         e.server.gameRules.set("doInsomnia", false)
         e.server.gameRules.set("doPatrolSpawning", false)
-        e.server.gameRules.set("doDaylightCycle", false)
+        e.server.gameRules.set("doWeatherCycle", false)
         e.server.gameRules.set("keepInventory", true)
         e.server.gameRules.set("disableRaids", true)
         e.server.gameRules.set("playersSleepingPercentage", 50)
