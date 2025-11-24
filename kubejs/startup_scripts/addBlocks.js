c0cb3988047d379e5995a87cf320fd8c83cee744
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2025-11-24 00:14:35 -0600

    Shop now handled by society trading

---
+++ kubejs/startup_scripts/addBlocks.js
@@ -0,0 +1,6 @@
+StartupEvents.registry('block', event => {
+    event.create('splendid_wares_shop')
+      .displayName('Splendid Wares Shop') 
+      .tagBlock('minecraft:mineable/pickaxe')
+      .soundType('stone')
+  })
